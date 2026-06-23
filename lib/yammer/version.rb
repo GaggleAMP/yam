df6844b9e7709507f8639dc6265656ad75a94eba
Gagan Lokesh <gaganlokesh@gmail.com>
2026-06-23 16:20:05 +0530

    Bump yam version to 2.6.0.rc1
    [GAG-11696]

--- lib/yammer/version.rb
+++ lib/yammer/version.rb
@@ -15,9 +15,9 @@
 module Yammer
   class Version
     MAJOR = 2 unless defined? Yammer::MAJOR
-    MINOR = 5 unless defined? Yammer::MINOR
-    PATCH = 1 unless defined? Yammer::PATCH
-    PRE = nil unless defined? Yammer::PRE
+    MINOR = 6 unless defined? Yammer::MINOR
+    PATCH = 0 unless defined? Yammer::PATCH
+    PRE = 'rc1' unless defined? Yammer::PRE
 
     class << self
 
